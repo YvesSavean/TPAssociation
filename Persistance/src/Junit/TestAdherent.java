d9d06a0d95c8d5e76491999eb303d4774f0d025e
E137013C <E137013C@U-INF-J-E113-08.iut-nantes.univ-nantes.prive>
2014-01-29 15:16:08 +0100

    Ajout jars + Junit

--- Persistance/src/Junit/TestAdherent.java
+++ Persistance/src/Junit/TestAdherent.java
@@ -2,10 +2,14 @@ package Junit;
 
 import static org.junit.Assert.*;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+
 import org.junit.Test;
 
 public class TestAdherent {
-
+	private final static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
