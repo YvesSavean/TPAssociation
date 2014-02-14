7c58acf2f8f91facdb9602751f2246f64fc721d7
alan-lb <alan.lb@hotmail.fr>
2014-02-14 11:55:19 +0100

    Ajout CommandeEntityManager et CommandeArticleEntityManager

---
+++ Persistance/src/EntityManager/CommandeEntityManager.java
@@ -0,0 +1,62 @@
+package EntityManager;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import Bean.Adherent;
+import Bean.Commande;
+
+public class CommandeEntityManager {
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private EntityTransaction t;
+
+	public CommandeEntityManager() {
+		emf = Persistence.createEntityManagerFactory("Persistance");
+		em = (EntityManager) emf.createEntityManager();
+		t = em.getTransaction();
+	}
+
+	public Commande trouver(int id) {
+		return em.find(Commande.class, id);
+	}
+
+	public void supprimer(Commande commande) {
+		// pour supprimer une commande, il faut au préalable supprimer les
+		// articles de la commande (table COMMANDE_ARTICLE) car existence d'une
+		// clé étrangère vers la table COMMANDE
+		t.begin();
+		em.remove(commande);
+		t.commit();
+	}
+
+	public void supprimer(int id) {
+		t.begin();
+		em.remove(id);
+		t.commit();
+	}
+
+	public void creer(Commande commande) {
+		t.begin();
+		em.persist(commande);
+		t.commit();
+	}
+
+	public void modifier(Commande commande) {
+		t.begin();
+		em.merge(commande);
+		t.commit();
+	}
+
+	public List<Commande> chercherCommandeDunAdherent(Adherent adh) {
+		Query query = em.createQuery("from Commande where lAdherent = " + adh
+				+ "");
+		return query.getResultList();
+	}
+
+}
