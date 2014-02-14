6f194cc96dd076af09d5ef3bf7f7c438a9a34f76
MikaelPhilip <philip.mikael@gmail.com>
2014-02-14 19:12:55 +0100

    Dynamique: Relecture du code et mise en forme des commentaires
    Des TODO ont été placé pour indiquer ou il faut "réactiver" le code une
    fois les méthodes demandées implémentées.

--- Dynamique/src/org/licence/pro/Action.java
+++ Dynamique/src/org/licence/pro/Action.java
@@ -17,14 +17,14 @@ import EntityManager.ArticleEntityManager;
 
 /**
  * Servlet implementation class Action
- * Servlet controleur récuperation de données et création de paramêtre
+ * Servlet controleur/traitement: récuperation des données depuis la JPA et création de paramêtre qu'on envoit à la vue
  */
 public class Action extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	/**Listes des objets pour l'acces aux données*/
 	private AdherentEntityManager adherentManagers;
 	private ArticleEntityManager articleManagers;
-	//private CommandeEntityManager commandeManager;
+	//TODO a activer: private CommandeEntityManager commandeManager;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -34,12 +34,14 @@ public class Action extends HttpServlet {
     }
 
     /**
-     * Initalisation des données
+     * Initalisation des EntityManagers utilisé pour manipuler les donnees
      */
     public void init(){
     	adherentManagers = new AdherentEntityManager();
     	articleManagers = new ArticleEntityManager();
+    	//TODO a activer: commandeManager = new CommandeManager();
     }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -54,27 +56,31 @@ public class Action extends HttpServlet {
 		process(request,response);
 	}
 	
+	/**
+	 * Methode qui réalise tout les traitements 
+	 */
 	private void process(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		
 		/*Chargement des objets pour acceder aux données*/
 		init();	
+		
+		
 		/*Gestion des session*/
-		//Si on vient de remplir le formulaire
 		//Si on vient de remplir le formulaire de connection
-
 		if ((request.getParameter("login")!=null) && (request.getParameter("mdp") != null)) {
 			if(adherentManagers != null){
+				//Si le login correspond bien à un adhérent
 				if( adherentManagers.trouver(request.getParameter("login"))!= null){
 					Adherent ad = adherentManagers.trouver(request.getParameter("login"));
-					//Vérifier le mdp
+					//On vérifie le mdp
 					if (request.getParameter("mdp").equals(ad.getMotDePasse())){
 						//On enregistre en session un attribut "login" cela signifie que la personne est connecté
 						request.getSession(true).setAttribute("login", new String(request.getParameter("login")));
 					}else{
+						//On rajoute dans un paramêtre un message d'erreur: Sera affiché par la vue pour informer l'utilisateur
 						request.setAttribute("Erreur", "Connection refusé:  Mdp incorrect");	
 					}
 				}else{
-					//On rajute dans un paramêtre un message d'erreur
 					request.setAttribute("Erreur", "Connection refusé: Identifiant inconnu");
 				}		
 			}else{
@@ -87,8 +93,8 @@ public class Action extends HttpServlet {
 		}
 		
 		/*Gestion des pages*/
-		//Suivant le chemin retourné par le template on va créer un attribut page que le template utilisera pour savoir quoi choisir
-		//les href du menu seront comme cela /action et on les récuperera avec les methode get path...
+		//Suivant le chemin retourné par le template, on va créer un attribut page que le template utilisera
+		//les href du menu sont comme cela ".../accueil" et on les récuperera avec les methode get path...
 		if(request.getPathInfo() != null){
 			request.setAttribute("Page", request.getPathInfo());
 		}
@@ -97,46 +103,56 @@ public class Action extends HttpServlet {
 		/*Gestion des données*/
 		//On vérifie la page que l'on a visé
 		if(request.getAttribute("Page")!= null){
+			
+			//On vise la page commande
 			if (request.getAttribute("Page").equals("/commande")){
-				System.out.println("Liste Commande");
 				//Liste pour récuperer les codes de tous les articles commandés
 				List<String> articlesCommandes = new ArrayList<String>();
 				//Liste pour récuperer l'ensemble des articles commandés
 				List<Article> articles = new ArrayList<Article>();
-				//En utilisant la persistance appeller une méthode qui renvoit pour un login adhérent l'ensemble des codes articles commandés
-				//articlesCommandes = commandeManager.listeCommande(request.getSession(true).getAttribute("login");
-				//Test
+				
+				//En utilisant la persistance,on appelle une méthode qui renvoit pour un login adhérent l'ensemble des codes articles commandés
+				//TODO a activer:articlesCommandes = commandeManager.listeCommande(request.getSession(true).getAttribute("login");
+				//Test temporaire à virer
 				articlesCommandes.add("aq");
 				articlesCommandes.add("ec");
-				//Pour chaque code d'article récuperer trouver son code le prix et le nom avec l'appel d'une méthode de jpa
+				
+				//Pour chaque code d'article récuperé on cherche l'article qui le concerne avec la JPA pour avoir toutes les infos
 				for (String code: articlesCommandes){
 					Article a = articleManagers.trouver(code);
-					//On enregistre dans une liste
-					articles.add(a);
+					//Si a n'est pas null
+					if(a != null){
+						//On enregistre dans une liste
+						articles.add(a);
+					}
 				}
-				//Si pas null alors on enregistre cette liste dans un attribut
+				
+				//Si la liste des articles retrouvés n'est pas null alors on enregistre cette liste dans un attribut
 				if(articles != null && !articles.isEmpty()){
 					request.setAttribute("articlesCommandes", articles);
 				}	
 			}
+			
+			//On vise la page des articles
 			if (request.getAttribute("Page").equals("/articles")){
-				System.out.println("Liste Articles");
-				//Crée une liste de type commandes_articles
+				//On crée une liste de type commandes_articles
 				List<Article> articles = new ArrayList<Article>();
 				//En utilisant la persistance appeller une méthode qui renvoit tout les articles disponibles
 				articles = articleManagers.ChercherArticles();
-				//Si pas null ou vide alors on enregistre cette liste dans un attribut
+				//Si articles n'est pas null ou vide alors on enregistre cette liste dans un attribut
 				if(articles != null && !articles.isEmpty()){
 					request.setAttribute("articles", articles);
 				}	
 			}
 		}
 		
+		
 		/*Gestion des actions*/
-		//L'ensemble des actions particuliéres (création porfile,ajout article seront traité ici).
-		//Cas on crée un compte page newAccount 
+		//L'ensemble des actions particuliéres (création porfile,ajout article) seront traité ici.
+		
+		//Cas on crée un compte (page newAccount) 
 		if(request.getParameter("newCreate")!=null){
-			//Vérifier que les champs obligatoire sont remplit (cas ou le html5 avec required ne marche pas sur le navigateur)
+			//Vérifier que les champs obligatoire sont remplit (au cas ou le html5 avec required ne marche pas sur le navigateur)
 			if ((!request.getParameter("newLogin").equals("")) && (!request.getParameter("mdp").equals("")) 
 					&& (!request.getParameter("mdpconfirm").equals("")) && (!request.getParameter("newNom").equals(""))
 					&& (!request.getParameter("newPrenom").equals("")) && (!request.getParameter("newAdresse").equals(""))
@@ -159,9 +175,10 @@ public class Action extends HttpServlet {
 							adh.setPays(request.getParameter("newPays"));
 							
 							//Données optionelles
-							if (request.getParameter("newCompAdresse")!=null || request.getParameter("newCompAdresse").equals(" ")){
+							if (request.getParameter("newCompAdresse")!=null || !request.getParameter("newCompAdresse").equals(" ")){
 								adh.setComplementAdresse(request.getParameter("newCompAdresse"));
 							}
+							
 							//Puis appel d'une méthode dans le jpa pour ajouter un adhérent
 							adherentManagers.creer(adh);
 						}else{
@@ -174,25 +191,25 @@ public class Action extends HttpServlet {
 				request.setAttribute("Erreur", "Veuillez remplir tout les champs");
 			}
 		}
-		//Cas on annule le création d'un compte
+		
 		//Cas on supprime les commandes
 		if(request.getParameter("annulerCmd")!=null){
-			System.out.println("Annuler commande");
-			//Liste pour récuperer les codes de tous les articles commandés (nécessaire pour faire la maj du stock
+			//Liste pour récuperer les codes de tous les articles commandés (nécessaire pour faire la maj du stock)
 			List<String> articlesCommandes = new ArrayList<String>();
-			//Recuperer l'ensembles des codes des articles commandés
-			//List<String> articlesCommandes = commandeManager.listeCommande(request.getSession(true).getAttribute("login"));
+			//Récuperer l'ensembles des codes des articles commandés
+			//TODO a activer:List<String> articlesCommandes = commandeManager.listeCommande(request.getSession(true).getAttribute("login"));
 			//Si la liste de commandes n'est pas vide ou null
 			if(articlesCommandes != null && !articlesCommandes.isEmpty()){
 				//Supression de la liste des commandes pour un adhérent donnée
-				//commandeManager.viderCommande(request.getSession(true).getAttribute("login"));
+				//TODO a activer:commandeManager.viderCommande(request.getSession(true).getAttribute("login"));
 				//Maj du stock des articles indiqués en parametres
 				for (String code: articlesCommandes){
-					//articleManagers.incrementeStock(code);
+					//TODO a activer:articleManagers.incrementeStock(code);
 				}
 			}
 			
 		}
+		
 		//Cas on rajoute une commande
 		if(request.getParameter("addCmd")!=null){
 			System.out.println("Ajout article dans commande");
@@ -204,33 +221,19 @@ public class Action extends HttpServlet {
 				//Vérifier que le stock articles n'est pas égal à zero
 				if(art.getStock() > 0){
 					//On decremente son stock (appel d'une méthode de jpa)
-					//articleManagers.decrementeStock(request.getParameter("addCmd"));
+					//TODO a activer:articleManagers.decrementeStock(request.getParameter("addCmd"));
 					//On rajoute cette article dans la liste des commandes 
-					//commandeManager.ajoutArticleCommande(request.getParameter("addCmd"));
+					//TODO a activer:commandeManager.ajoutArticleCommande(request.getParameter("addCmd"));
 				}else{
 					request.setAttribute("Erreur", "L'article n'est plus disponible");
 				}
 			}
-			
-			
-			//TODO:Si true : message d'erreur sinon on appelle une méthode addArtcle (id Adehrent, id article) qui rajoute l'article dans la liste des commandes de l'adherent
 		}
 		
 		
 		
 		//Appel du template
-		getServletContext().getRequestDispatcher("/template.jsp").forward(request, response);
-		
-		//Convertir template en JSTL OK
-		//Finir les templates (utiliser JSTL + EL) OK
-		//TODO:Améliorer site: 
-			//TODO:Liens par défault
-			//Bloquer accées direct jsp OK
-			//Bouton annuler dans crée compte OK
-		//JPA: mise en place de la structure OK
-		//Completer code JPA créer compte: rajouter tyoe password + complément adresse
-		//Implementer les méthodes jpa
-		
+		getServletContext().getRequestDispatcher("/template.jsp").forward(request, response);	
 	}
 	
 }
